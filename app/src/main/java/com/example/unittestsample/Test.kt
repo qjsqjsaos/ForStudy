a73b7aa611530e6d44f300d51209af1007881781
qjsqjsaos <merrygoaround0726@gmail.com>
2022-04-26 18:59:39 +0900

    learn about "When"

--- app/src/main/java/com/example/unittestsample/Test.kt
+++ app/src/main/java/com/example/unittestsample/Test.kt
@@ -51,32 +51,63 @@ fun main() {
 //    println("myNum is ${--myNum}")
 
 
-    var heightPerson1 = 170
-    var heightPerson2 = 159
+//    var heightPerson1 = 170
+//    var heightPerson2 = 159
+//
+//    if(heightPerson1 > heightPerson2) {
+//        println("use raw force")
+//    } else if (heightPerson1 == heightPerson2) {
+//        println("use your power technique 1337")
+//    } else {
+//        println("use technique")
+//    }
+//
+//    val age = 31
+//
+//    if(age >= 30)
+//        println("you're over 30")
+//
+//
+//
+//    if (age >= 21) {
+//        println("now you may drink in the US")
+//    } else if(age >= 18) {
+//        println("you may vote now")
+//    } else if(age >= 16) {
+//        println("you may drive now")
+//    } else {
+//        println("you're too young")
+//    }
 
-    if(heightPerson1 > heightPerson2) {
-        println("use raw force")
-    } else if (heightPerson1 == heightPerson2) {
-        println("use your power technique 1337")
-    } else {
-        println("use technique")
+//    var name = "Denis"
+//    if(name == "Denis") {
+//        println("welcome home denis")
+//    }
+//    else {
+//        println("Who are you?")
+//    }
+//
+//    var isRainy = true
+//    if(isRainy)
+//        println("It's Rainy")
+
+    var season = 3
+    when(season) {
+        1 -> println("Spring")
+        2 -> println("Summer")
+        3 -> {
+            println("Fall")
+            println("Autumn")
+        }
+        4 -> println("Winter")
+        else -> println("Invalid Season")
     }
 
-    val age = 31
-
-    if(age >= 30)
-        println("you're over 30")
-
-
-
-    if (age >= 21) {
-        println("now you may drink in the US")
-    } else if(age >= 18) {
-        println("you may vote now")
-    } else if(age >= 16) {
-        println("you may drive now")
-    } else {
-        println("you're too young")
+    var month = 3
+    when(month) {
+        in 3..5 -> println("Spring")
+        in 6..8 -> println("Summer")
+        in 9..11 -> println("Fall")
+        in 12 downTo 2 -> println("Winter")
     }
-
 }
