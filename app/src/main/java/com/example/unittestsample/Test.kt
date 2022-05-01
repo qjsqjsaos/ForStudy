bdda1f02eeb266b32cd5c9cffacd294cdbb8e6ae
souyeolBaek <merrygoaround0726@gmail.com>
2022-05-01 22:20:27 +0900

    learn about "nullable"

--- app/src/main/java/com/example/unittestsample/Test.kt
+++ app/src/main/java/com/example/unittestsample/Test.kt
@@ -1,172 +1,101 @@
-package com.example.unittestsample
-
-fun main() {
-//    var courseName :String = "Android Masterclass"
-//    val leet : Float = 13.37F
-//    val pi : Double = 3.14159265358979
-//    var age : Byte = 25
-//    var year : Short = 2020
-//    var phoneNumber: Long = 18881234567
-//    var isGood : Boolean = true
-//    var firstCharacter : Char = 'a'
-
-    val myStr = "Hello World"
-    var firstCharInStr = myStr[0]
-    var lastCharInStr = myStr[myStr.length - 1]
-    var myLength = myStr.length
-//    print("First character $firstCharInStr and the length of myStr is ${myStr.length}")
-
-    //Arirthmetic operators (+, -, *, /, %)
-    var result = 5 + 3
-    val a = 1.0
-    val n = 2.0
-    val reusltDouble: Double = a / n
-//    print(reusltDouble)
-
-    //Comparison operators (==, !=, <, >, <=, >=)
-    val isEqual = 5 == 3
-//    println("isEqual is $isEqual")
+Code: Nullables
+Please find the Code for Nullables here:
 
 
-    val isNotEqual = 5 != 5
-    //String interpolation
-//    println("isNotEqual is $isNotEqual")
-//
-//    println("is-5greater3 ${-5 > 3}")
-//    println("is5LowerEqual3 ${5 <= 3}")
-//    println("is5GreaterEqual3 ${5 >= 3}")
 
-    //Assignment operators (+=, -=, *=, /=, %=)
-    var myNum = 5
-//    myNum = myNum + 3
-    myNum += 3
-    myNum *= 4
-//    println("myNum is $myNum")
+package eu.tutorials.helloworld
 
-    //Increment =& Decrement operators (++, --)
-    myNum++
-//    println("myNum is $myNum")
-//    println("myNum is ${myNum++}")
-//    println("myNum is ${++myNum}")
-//    println("myNum is ${--myNum}")
+fun main(){
+    // NULLABLES/OPTIONALS in Kotlin
+// Kotlin supports nullability as part of its type System.
+// That means You have the ability to declare whether 
+// a variable can hold a null value or not.
+// By supporting nullability in the type system,
+// the compiler can detect 
+// possible NullPointerException errors at compile time 
+// and reduce the possibility of having them thrown at runtime.
+    var name: String = "Denis"
+// name = null // Compilation Error
 
+    var nullableName: String? = "Denis"
+    nullableName = null // Works
 
-//    var heightPerson1 = 170
-//    var heightPerson2 = 159
-//
-//    if(heightPerson1 > heightPerson2) {
-//        println("use raw force")
-//    } else if (heightPerson1 == heightPerson2) {
-//        println("use your power technique 1337")
-//    } else {
-//        println("use technique")
-//    }
-//
-//    val age = 31
-//
-//    if(age >= 30)
-//        println("you're over 30")
-//
-//
-//
-//    if (age >= 21) {
-//        println("now you may drink in the US")
-//    } else if(age >= 18) {
-//        println("you may vote now")
-//    } else if(age >= 16) {
-//        println("you may drive now")
-//    } else {
-//        println("you're too young")
-//    }
-//    var age = 21
-//    when(age) {
-//        !in 0..20 -> println("now you may drink in the US")
-//        in 18..20 -> println("you may vote now")
-//        16, 17 -> println("you may drive now")
-//        else -> println("you're too young")
-//    }
+// Here name cannot/must not be null
+    val len = name.length
+    val upper = name.toLowerCase()
 
-//    var x : Any = "13.37f"
-//    when(x) {
-//        is Int -> println("$x is an Int")
-//        !is Double -> println("$x is not a Double")
-//        is String -> println("$x is a String")
-//        else -> println("$x is none of the above")
-//    }
+// but the same methods won't work on nullable types
+    val len2 = nullableName.length // Compilation Error
+    val upper2 = nullableName.toLowerCase()  // Compilation Error
 
-//    var name = "Denis"
-//    if(name == "Denis") {
-//        println("welcome home denis")
-//    }
-//    else {
-//        println("Who are you?")
-//    }
-//
-//    var isRainy = true
-//    if(isRainy)
-//        println("It's Rainy")
+// So how can we solve this? We could do a null check before hand
 
-//    var season = 3
-//    when(season) {
-//        1 -> println("Spring")
-//        2 -> println("Summer")
-//        3 -> {
-//            println("Fall")
-//            println("Autumn")
-//        }
-//        4 -> println("Winter")
-//        else -> println("Invalid Season")
-//    }
-//
-//    var month = 3
-//    when(month) {
-//        in 3..5 -> println("Spring")
-//        in 6..8 -> println("Summer")
-//        in 9..11 -> println("Fall")
-//        in 12 downTo 2 -> println("Winter")
-//    }
+    val nullableName2: String? = "Denis"
+
+    if(nullableName2 != null) {
+        println("Hello, ${nullableName2.toLowerCase()}.")
+        println("Your name is ${nullableName2.length} characters long.")
+    } else {
+        println("Hello, Guest")
+    }
+
+// This works but seems to be quite some work...
+// So how about we shorten the syntax...
+// Kotlin provides a Safe call operator, ?.  
+// It allows you to combine a null-check and 
+// a method call in a single expression.
+
+    nullableName2?.toLowerCase()
+
+// This is the same as:
+    if(nullableName2 != null)
+        nullableName2.toLowerCase()
+    else
+        null
+
+// You can use methods on a nullable variable like this
+    val nullableName3: String? = null
+
+    println(nullableName3?.toLowerCase()) // prints null
+    println(nullableName3?.length) // prints null
+
+// You can perform a chain safe calls:
+    //val wifesAge: String? = user?.wife?.age
 
 
-    //While loop
-//    var x = 100
-//    while(x >= 0) {
-//        print("$x")
-//        x -= 2
-//    }
-//    println("\n while loop is done")
-//
-//    x = 15
-//    do {
-//        print("$x")
-//        x++
-//    }while (x <= 10)
-//    println("\n do while loop is done")
-//
-//    var feltTemp = "cold"
-//    var roomTemp = 10
-//    while (feltTemp == "cold") {
-//        roomTemp++
-//        if(roomTemp >= 20) {
-//            feltTemp = "comfy"
-//            println("it's comfy now")
-//        }
-//    }
+// Let'S say we don’t want to print anything if 
+// the variable is null?
 
-//    for(num in 1..10) {
-//        print("$num")
-//    }
-//
-//    for(i in 1 until 10) { // Same as - for(i in 1.until(10))
-//        print("$i ")
-//    }
-//    println("-----------------------")
-//    for(i in 10 downTo 1 step 2) { // Same as - for(i in 10.downTo(1).step(2))
-//        print("$i ")
-//    }
+// In order to perform an operation only if the 
+// variable is not null, we can use the safe call 
+// operator with let -
 
-    var z = 12
-    do {
-        print("$z ")
-    } while(z <= 10)
+    val nullableName4: String? = null
+
+    nullableName4?.let { println(it.toLowerCase()) }
+    nullableName4?.let { println(it.length) }
+// Prints nothing because there nullableName is null 
+// and we used let to prevent anything from being performed
+
+
+// What if we would like to enter a default value?
+// Then we can use the elvis operator ?:
+    val name2 = nullableName4 ?: "Guest"
+
+    //val wifesAge2: String? = user?.wife?.age ?: 0
+
+
+// Not null assertion : !! Operator
+// The !! operator converts a nullable type to a 
+// non-null type, and throws a NullPointerException 
+// if the nullable type holds a null value.
+// This is risky, and you should only use it if 
+// you are 100% certain, that there will be a value in 
+// the variable.
+    val nullableName5: String? = null
+    nullableName5!!.toLowerCase() // Results in NullPointerException
 
 }
+ 
+ 
+ 
